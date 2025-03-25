84393c2904deb9ca31e1f6888298a2ed91938fee
sfab07 <sfab07@gmail.com>
2025-03-25 12:13:27 +0100

    Create stations2.sh
    Signed-off-by: sfab07 <sfab07@gmail.com>

---
+++ stations2.sh
@@ -0,0 +1,1 @@
+cd; wget -c -t 0 https://raw.githubusercontent.com/sfab07/stations.xml-Goodvibes/refs/heads/main/stations.xml ; mv stations.xml ~/.var/app/io.gitlab.Goodvibes/data/goodvibes/stations.xml
